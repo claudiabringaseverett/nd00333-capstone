f438316574ea9b938cb8140c8fc2dac5124b397d
Claudia Everett <40345934+claudiabringaseverett@users.noreply.github.com>
2021-04-18 20:34:04 -0400

    Update train.py

--- starter_file/train.py
+++ starter_file/train.py
@@ -37,10 +37,12 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
 
     os.makedirs("outputs",exist_ok=True)
+    
     joblib.dump(value=model,filename="./outputs/model.joblib")
+    
+    run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
     main()
